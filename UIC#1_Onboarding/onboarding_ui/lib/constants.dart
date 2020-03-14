1243674974a4cb787fdfc4c2c6d8e1bc3f7d841c
rkv31 <58583307+rkv31@users.noreply.github.com>
2020-03-14 23:16:23 +0530

    onboarding_ui

---
+++ UIC#1_Onboarding/onboarding_ui/lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kDialogueTextStyle = TextStyle(
+  color: Color(0xFF111328),
+  fontSize: 42.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kContentTextStyle = TextStyle(
+  color: Color(0xFF111328),
+  fontSize: 20.0,
+);
+
+const kDarkBlue = Color(0xFF111328);
