e31e083a7e81c7db458525ce5a26e9460462658f
Nikhil Nayak <nikhil.nixel@gmail.com>
2020-04-15 20:23:15 +0530

    Update

--- UIC#4_Rive/rive_animted_ui/lib/pages/HomePage.dart
+++ UIC#4_Rive/rive_animted_ui/lib/pages/HomePage.dart
@@ -154,66 +154,73 @@ class _MyHomePageState extends State<MyHomePage>
                 child: SizedBox(
                   height: MediaQuery.of(context).size.height * 0.48,
                   width: MediaQuery.of(context).size.width,
-                  child: Swiper(
-                    duration: 100,
-                    physics: BouncingScrollPhysics(),
-                    itemCount: prefs == null
-                        ? 0
-                        : (prefs.getStringList("dataKeys") ?? []).length,
-                    viewportFraction: 0.74,
-                    scale: 0.81,
-                    scrollDirection: Axis.horizontal,
-                    loop: false,
-                    itemBuilder: (BuildContext context, int index) {
-                      List<List<String>> data = [];
-                      print("Rebuilding Swiper...");
-                      if (prefs.containsKey('dataKeys')) {
-                        prefs.getStringList("dataKeys").forEach((element) {
-                          data.add(prefs.getStringList(element));
-                        });
-                      }
+                  child: prefs != null &&
+                          prefs.getStringList("dataKeys").length != 0
+                      ? Swiper(
+                          duration: 100,
+                          physics: BouncingScrollPhysics(),
+                          itemCount: prefs == null
+                              ? 0
+                              : (prefs.getStringList("dataKeys") ?? []).length,
+                          viewportFraction: 0.74,
+                          scale: 0.81,
+                          scrollDirection: Axis.horizontal,
+                          loop: false,
+                          itemBuilder: (BuildContext context, int index) {
+                            List<List<String>> data = [];
+                            print("Rebuilding Swiper...");
+                            if (prefs.containsKey('dataKeys')) {
+                              prefs
+                                  .getStringList("dataKeys")
+                                  .forEach((element) {
+                                data.add(prefs.getStringList(element));
+                              });
+                            }
 
-                      return Hero(
-                        key: Key(index.toString()),
-                        tag: "edit-button-$index",
-                        child: Material(
-                          key: Key(index.toString()),
-                          color: Colors.transparent,
-                          child: CardView(
-                            data: data[index],
-                            id: index,
-                          ),
-                        ),
-                      );
-                    },
+                            return Hero(
+                              key: Key(index.toString()),
+                              tag: "edit-button-$index",
+                              child: Material(
+                                key: Key(index.toString()),
+                                color: Colors.transparent,
+                                child: CardView(
+                                  data: data[index],
+                                  id: index,
+                                ),
+                              ),
+                            );
+                          },
+                        )
+                      : EmptyIndicationCard(),
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Hero(
+                    tag: "add-button",
+                    child: Container(
+                      height: 66,
+                      width: 66,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(33),
+                      ),
+                      child: GestureDetector(
+                        child: Icon(Icons.add, size: 40),
+                        onTap: () async {
+                          await Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => EditPage(autoGen: true)),
+                          );
+                          setState(() {
+                            prefs.getStringList("dataKeys");
+                          });
+                        },
+                      ),
+                    ),
                   ),
                 ),
               ),
-              Center(
-                child: Hero(
-                  tag: "add-button",
-                  child: Container(
-                    height: 66,
-                    width: 66,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(33),
-                    ),
-                    child: GestureDetector(
-                      child: Icon(Icons.add, size: 40),
-                      onTap: () async {
-                        await Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (context) => EditPage(autoGen: true)),
-                        );
-                        setState(() {
-                          prefs.getStringList("dataKeys");
-                        });
-                      },
-                    ),
-                  ),
-                ),
-              )
             ],
           ),
         ],
